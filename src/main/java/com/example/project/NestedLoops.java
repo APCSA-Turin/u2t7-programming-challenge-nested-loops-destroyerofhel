e2dff2b6156d2d38dcb8b88c35dbe2ef4e12dfb4
destroyerofhel <156379798+destroyerofhel@users.noreply.github.com>
2025-12-10 18:44:48 +0000

    hello

--- src/main/java/com/example/project/NestedLoops.java
+++ src/main/java/com/example/project/NestedLoops.java
@@ -17,7 +17,14 @@ return the string below for given height=5
 *****  
            */
 public static String starStaircase(int height){
-    return "";
+    String staircase = "";
+    for(int i = 1; i <= height; i++) {
+        for(int j = 1; j <= i; j++) {
+            staircase += "*";
+        }
+        if(i != height) staircase += "\n";
+    }
+    return staircase;
 }
           /*
 return the string below for given height=5
@@ -29,7 +36,14 @@ return the string below for given height=5
            */
 
 public static String starStaircaseReverse(int height){
-    return "";
+    String staircase = "";
+    for(int i = height; i > 0; i--) {
+        for(int j = 1; j <= i; j++) {
+            staircase += "*";
+        }
+        if (i != 1) staircase += "\n";
+    }
+    return staircase;
 }
             /*
 
@@ -41,7 +55,23 @@ width=5 height=5
 *****
           */
     public static String emptyBox(int width, int height) {
-        return "";
+        if(width == 1 || height == 1) return "";
+        String box = "";
+        for(int i = 0; i < height; i++) {
+            for(int j = 0; j < width; j++) {
+                if(i == 0 || i == height-1) {
+                    box += "*";
+                }
+                else if(j == 0 || j == width-1) {
+                    box += "*";
+                }
+                else {
+                    box += " ";
+                }
+            }
+            if(i != height-1) box += "\n";
+        }
+        return box;
     }
                     /*
 return the string below for word="HELLO" rows=5                    
@@ -54,7 +84,18 @@ OHELL
   i will provide a hint for this one if you get stuck.. use modulo
                 */
     public static String repeatRectangle(String word, int rows){
-        return "";
+        String rectangle = word + "\n";
+        if(rows == 1) return word;
+        String lastLine = word;
+        for(int i = 1; i < rows; i++) {
+            String line = "";
+            for(int j = 1; j <= word.length(); j++) {
+                line += String.valueOf(lastLine.charAt(j % word.length()));
+            }
+            lastLine = line;
+            if(i != rows-1) line += "\n";
+            rectangle += line;
+        }
+        return rectangle;
     }
-
 }
